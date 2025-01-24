0964e524662f3d7466a6d6ac5b798b0772ef4936
tamyresrsm <tamyres.rsmagalhaes@senacsp.edu.br>
2025-01-23 21:23:25 -0300

    feat: Usando querySelector ára Capturar Valores no HTML

--- js/objeto.js
+++ js/objeto.js
@@ -1,21 +1,52 @@
-//let nome = prompt("Informe Nome");
-//let altura = parseFloat(prompt("Informe Altura:"))
-//let idade = parseInt(prompt("Informe Idade:"));
-//let dataNascimento = prompt("Informe Data Nascimento (dd/MM/yyyy");
+let botao = document.querySelector("#btnMostrar");
+botao.addEventListener("click", mostrarValores);
 
 
-let pessoa = {};
-pessoa.nome = prompt("Informe Nome");
-pessoa.altura = parseFloat(prompt("Informe Altura:"));
-pessoa.idade = parseInt(prompt("Informe Idade:"));
-pessoa.dataNascimento = prompt(
-    "Informe Data Nascimento (dd/MM/yyyy");
+function mostrarValores() {
+    let pessoa = {};
+
+    pessoa.nome =
+        document.querySelector('#txtNome').value;
+
+    pessoa.altura = parseFloat
+        (document.querySelector('#txtAltura').value);
+
+    pessoa.idade = parseInt
+        (document.querySelector('#txtIdade').value);
+
+    pessoa.dataNascimento = document.querySelector('#txtDataNascimento').value;
 
 
-console.log(pessoa);    
-alert(
-    `O nome digitado é ${pessoa.nome}
-     A altura informada é ${pessoa.altura.toFixed(2)}
-     A idade informada é ${pessoa.idade}
-     A data de nascimento informada é ${pessoa.dataNascimento}
-    `);
+    let mensagem = `
+   <br><br>
+     O nome digitado é ${pessoa.nome} </strong> <br>
+     A altura informada é ${pessoa.altura.toFixed(2)} </strong> <br>
+     A idade informada é ${pessoa.idade} </strong> <br>
+     A data de nascimento informada é ${pessoa.dataNascimento} </strong> `;
+
+    let div = document.querySelector("#resultado");
+    div.innerHTML = mensagem;
+
+}
+
+// let nome = prompt("Informe Nome");
+// let altura = parseFloat(prompt("Informe Altura:"))
+// let idade = parseInt(prompt("Informe Idade:"));
+// let dataNascimento = prompt("Informe Data Nascimento (dd/MM/yyyy");
+
+
+// let pessoa = {};
+// pessoa.nome = prompt("Informe Nome");
+// pessoa.altura = parseFloat(prompt("Informe Altura:"));
+// pessoa.idade = parseInt(prompt("Informe Idade:"));
+// pessoa.dataNascimento = prompt(
+//     "Informe Data Nascimento (dd/MM/yyyy");
+
+
+// console.log(pessoa);
+// alert(
+//     `O nome digitado é ${pessoa.nome}
+//      A altura informada é ${pessoa.altura.toFixed(2)}
+//      A idade informada é ${pessoa.idade}
+//      A data de nascimento informada é ${pessoa.dataNascimento}
+//     `);
